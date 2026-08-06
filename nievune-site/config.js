a0245ade5fda44bf5e14b5b3d6b3bf548d80b423
beladesign <belamral.isa@gmail.com>
2026-08-06 12:45:19 -0300

    Delete nievune-site directory

--- nievune-site/config.js
+++
@@ -1,9 +0,0 @@
-window.NIEVUNE_CONFIG = {
-  supabaseUrl: "https://hbwdvfzqxmoqdzirlwzn.supabase.co",
-  supabaseAnonKey: "sb_publishable_SrxylJ-qFUrPv3khtP2Heg_BUAUhWgp",
-  authRedirectUrl: "https://beladesign.github.io/nievune/",
-  socialAuth: { google: true, apple: false },
-  siteName: "Nievune",
-  currency: "USD",
-  socials: { twitch: "", x: "" }
-};
