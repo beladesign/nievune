e725e3087c39328ffcc5d7bd33d33bb96bf8326a
beladesign <belamral.isa@gmail.com>
2026-08-07 13:22:48 -0300

    Delete nievune-site directory

--- nievune-site/config.example.js
+++
@@ -1,9 +0,0 @@
-window.NIEVUNE_CONFIG = {
-  // Fill these two values after creating the Supabase project.
-  // The anon key is public by design; security is enforced by database policies.
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  siteName: "Nievune",
-  currency: "USD",
-  socials: { twitch: "", x: "" }
-};
